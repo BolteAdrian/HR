2b7140de98076163ab3c0f0df9335cd99e529017
Adrian - Paul Bolte <Bolte.Il.Adrian@student.utcluj.ro>
2021-10-25 18:04:16 +0300

    Aproape gata

--- Models/Multi.cs
+++ Models/Multi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -11,14 +12,18 @@ namespace HR.Models
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        [Required]
         public DateTime? InterviewDate { get; set; }
+        [Required]
         public string FunctionApply { get; set; }
         public string DepartamentApply { get; set; }
         public string EmployeeName { get; set; }
+        [Required]
         public bool? Accepted { get; set; }
         public string TestResult { get; set; }
         public int? RefusedReason { get; set; }
         public DateTime? DateAnswer { get; set; }
+        [Required]
         public int? OffertStatus { get; set; }
         public DateTime? EmploymentDate { get; set; }
     }
